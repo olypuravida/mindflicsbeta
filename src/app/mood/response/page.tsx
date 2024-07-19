87efb395e53999f325ed9c03f4c262a8b81e0317
Johan Smith <johan6smith@gmail.com>
2024-07-19 14:34:06 -0500

    Layout Response mood tracker

--- src/app/mood/response/page.tsx
+++ src/app/mood/response/page.tsx
@@ -13,6 +13,7 @@ import styles from './styles.module.scss'
 import { TitleWelcome } from '@/app/containers/sections/mindflics/title-welcome'
 import { SidebarRight } from '@/app/containers/sections/mindflics/sidebar-right'
 import { ResponseMoodTracker } from '@/app/containers/sections/mindflics/response-mood-tracker'
+import Typography from '@mui/material/Typography'
 
 export default function MoodResponse() {
 
@@ -35,6 +36,25 @@ export default function MoodResponse() {
 
             <Container className={ styles.container_mood_response }>
               <ResponseMoodTracker />
+
+              <Grid className={ styles.footer_response_mood_tracker }>
+                <Typography className={ styles.text_footer_response_mood_tracker }>
+                  Remember you can also create a new entry on your journal to develope more about it.
+                </Typography>
+
+                <svg
+                  fill="none"
+                  height="43"
+                  viewBox="0 0 50 43"
+                  width="50"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0 21.433a4 4 0 004 4h31.68L25.174 35.94a4 4 0 005.653 5.653L48.16 24.26a4 4 0 000-5.654L30.827 1.273a4 4 0 10-5.653 5.653L35.68 17.433H4c-2.208 0-4 1.792-4 4z"
+                    fill="#03989E"
+                  />
+                </svg>
+              </Grid>
             </Container>
 
           </Grid>
--- src/domain/providers/theme/shared/fonts/roboto.ts
+++ src/domain/providers/theme/shared/fonts/roboto.ts
@@ -10,6 +10,6 @@ import { Roboto } from 'next/font/google'
 export const roboto = Roboto({
   display: 'swap',
   subsets: ['latin'],
-  variable: '--font-roboto',
+  variable: '--font-poppins',
   weight: ['100', '300', '400', '500', '700', '900'],
 })
