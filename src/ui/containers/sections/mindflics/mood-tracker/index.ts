d035269cf425fbe82505d90b45dc83e9139532a5
Johan Smith <johan6smith@gmail.com>
2024-07-19 11:48:42 -0500

    Response mood tracker page

---
+++ src/ui/containers/sections/mindflics/mood-tracker/index.ts
@@ -0,0 +1,1 @@
+export * from './wheel1/Wheel'
---
+++ src/ui/containers/sections/mindflics/response-mood-tracker/ResponseMoodTracker.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React from 'react'
+import styles from './styles.module.scss'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+
+
+export function ResponseMoodTracker() {
+    return (
+      <Container className={ styles.container_response_mood_tracker }>
+        <Typography className={ styles.title_response_mood_tracker }>
+          Thanks for record your current mood
+        </Typography>
+
+        <Typography className={ styles.subtitle_response_mood_tracker }>
+          Based on your current mood, we recommend you to watch videos on 
+          {' '}
+
+          <b>#CategoryName</b>
+
+          {' '}
+          category, that could be helpfull
+        </Typography>
+
+        <Container className={ styles.content_btn_response_mood_tracker }>
+          <Button 
+            className={ styles.action_button } 
+            size="large"
+            variant="contained"
+          >
+            Show me
+          </Button>
+        </Container>
+      </Container>
+    )
+}
